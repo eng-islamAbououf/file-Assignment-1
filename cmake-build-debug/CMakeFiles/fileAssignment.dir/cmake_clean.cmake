ba10ae04e98ec2a90d5c02107490e1be948869a3
Eng Islam Abououf <eng.islam.abououf@gmail.com>
2021-11-23 23:39:47 +0200

    initail push

---
+++ cmake-build-debug/CMakeFiles/fileAssignment.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fileAssignment.dir/main.cpp.obj"
+  "fileAssignment.exe"
+  "fileAssignment.exe.manifest"
+  "fileAssignment.pdb"
+  "libfileAssignment.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fileAssignment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
